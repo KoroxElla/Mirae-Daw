29469c47a462abbe46243dd4f9a88bb5b8f55555
dom10 <dom10@student.le.ac.uk>
2026-04-06 01:26:51 +0100

    I used firebase to store the music files

--- Mirae-app/src/components/BackgroundMusic.tsx
+++ Mirae-app/src/components/BackgroundMusic.tsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useRef, useState } from 'react';
 
+
+const MUSIC_URLS = {
+  theme: "https://firebasestorage.googleapis.com/v0/b/daw-db.firebasestorage.app/o/music%2Ftheme.mp3?alt=media",
+  joy: "https://firebasestorage.googleapis.com/v0/b/daw-db.firebasestorage.app/o/music%2Fhappy.mp3?alt=media",
+  sadness: "https://firebasestorage.googleapis.com/v0/b/daw-db.firebasestorage.app/o/music%2Fsad.mp3?alt=media",
+  tense: "https://firebasestorage.googleapis.com/v0/b/daw-db.firebasestorage.app/o/music%2Ftense.mp3?alt=media"
+};
+
 export default function BackgroundMusic() {
   const audioRef = useRef<HTMLAudioElement | null>(null);
   const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
-    audioRef.current = new Audio('/music/theme.mp3');
+    audioRef.current = new Audio(MUSIC_URLS.theme);
     audioRef.current.loop = true;
     
     const handleVolumeChange = (e: CustomEvent) => {
