9a3577c668fc31a8a32e2dfe5faf9a13feec4bf3
dom10 <dom10@student.le.ac.uk>
2026-04-09 13:30:20 +0100

    Forgot to import useRef in Scene.tsx

--- Mirae-app/src/components/Scene.tsx
+++ Mirae-app/src/components/Scene.tsx
@@ -1,5 +1,5 @@
 import { useGLTF } from "@react-three/drei";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import * as THREE from "three";
 
 interface SceneProps {
