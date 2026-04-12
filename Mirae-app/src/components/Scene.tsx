3e206c77787525ac469cfa712185b7c85407626a
dom10 <dom10@student.le.ac.uk>
2026-04-12 20:24:13 +0100

    Tried an extreme incrememnt to see if there'll be any change

--- Mirae-app/src/components/Scene.tsx
+++ Mirae-app/src/components/Scene.tsx
@@ -29,7 +29,7 @@ export default function Scene({ url, onLoad }: SceneProps) {
     box.getCenter(center);
 
     cloned.position.sub(center);
-    cloned.position.y += 3;
+    cloned.position.y -= 33;
 
     const maxDim = Math.max(size.x, size.y, size.z);
     const scale = 2 / maxDim;
