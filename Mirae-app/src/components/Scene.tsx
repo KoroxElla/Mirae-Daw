764390c87be529e5d98f05baa8b50375e9aeec8d
KoroxElla <146519293+KoroxElla@users.noreply.github.com>
2026-04-12 23:55:05 +0100

    Adjust vertical position offset in Scene component

--- Mirae-app/src/components/Scene.tsx
+++ Mirae-app/src/components/Scene.tsx
@@ -29,7 +29,7 @@ export default function Scene({ url, onLoad }: SceneProps) {
     box.getCenter(center);
 
     cloned.position.sub(center);
-    cloned.position.y +=55;
+    cloned.position.y +=50;
     cloned.position.x +=10.5;
     cloned.position.z +=3.5
 
