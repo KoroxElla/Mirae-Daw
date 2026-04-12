600afdf618d1c694c289acb5196c1f91bab44751
KoroxElla <146519293+KoroxElla@users.noreply.github.com>
2026-04-13 00:25:34 +0100

    Adjust position and scale calculations in Scene.tsx

--- Mirae-app/src/components/Scene.tsx
+++ Mirae-app/src/components/Scene.tsx
@@ -29,12 +29,12 @@ export default function Scene({ url, onLoad }: SceneProps) {
     box.getCenter(center);
 
     cloned.position.sub(center);
-    cloned.position.y +=45;
+    cloned.position.y +=40;
     cloned.position.x +=10.5;
     cloned.position.z +=3.5
 
     const maxDim = Math.max(size.x, size.y, size.z);
-    const scale = (2 / maxDim) * 3;
+    const scale = (2 / maxDim) * 5;
     cloned.scale.setScalar(scale);
 
     cloned.traverse((child) => {
