f6412ed3e5ab965b62d14fcda3a10b1522eee3f0
KoroxElla <146519293+KoroxElla@users.noreply.github.com>
2026-04-13 00:46:02 +0100

    Adjust vertical position of cloned object

--- Mirae-app/src/components/Scene.tsx
+++ Mirae-app/src/components/Scene.tsx
@@ -29,7 +29,7 @@ export default function Scene({ url, onLoad }: SceneProps) {
     box.getCenter(center);
 
     cloned.position.sub(center);
-    cloned.position.y +=40;
+    cloned.position.y +=38;
     cloned.position.x +=10.5;
     cloned.position.z +=3.5
 
