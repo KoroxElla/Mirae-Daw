20d8a0d2194fe689c883ad65dc9a41385354f302
KoroxElla <146519293+KoroxElla@users.noreply.github.com>
2026-05-02 05:23:29 +0100

    Add SCENE_ADJUSTMENTS for scene configurations

---
+++ Mirae-app/src/types/sceneConfig.ts
@@ -0,0 +1,9 @@
+export const SCENE_ADJUSTMENTS = {
+  neutral:  { pos: [-8, -57.2, 0], scale: 1.2, rotY: Math.PI / 2 },
+  joy:      { pos: [-5, -3.7, 3], scale: 1.95, rotY: Math.PI / 2 * 3 },
+  sadness:  { pos: [-4, -0.85, 1.1], scale: 6.1, rotY: 0 },
+  anger:    { pos: [8, -8.98, 15], scale: 2.25, rotY: Math.PI / 2 * 3 },
+  fear:     { pos: [-20, 1.75, -45], scale: 0.025, rotY: 0 },
+  disgust:  { pos: [5, -0.65, -12.55], scale: 2.22, rotY: Math.PI / 2 * 3 },
+  surprise: { pos: [-5, -0.88, -3], scale: 0.5, rotY: 0 }
+};
